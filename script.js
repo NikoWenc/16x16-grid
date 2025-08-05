c9a01fd2aeb1c95039090526aa47292b97856d8f
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-05 22:22:10 +0800

    add styles and center the grid

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const mainContainer = document.querySelector("#main-container");
 
 function appendDivsTo(container){
-for (let i = 1; i <= 16; i++){
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "main-child");
-    container.appendChild(newDiv);
-}
+    for (let i = 1; i <= 256; i++){
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "main-child");
+        container.appendChild(newDiv);
+    }
 }
 appendDivsTo(mainContainer);
+
