ff64793dadeef30ea53da128b4ad63426c4b2586
NikoWenc <96979773+NikoWenc@users.noreply.github.com>
2025-08-06 14:31:53 +0800

    add a button to get grind counts

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const mainContainer = document.querySelector("#main-container");
+const button = document.querySelector("#box-count");
+let boxCount;
 
-function appendDivsTo(container){
-    for (let i = 1; i <= 256; i++){
+function appendDivsTo(container, counter = 256){
+    console.log(counter);
+    for (let i = 1; i <= counter; i++){
         const newDiv = document.createElement("div");
         newDiv.setAttribute("class", "main-child");
         container.appendChild(newDiv);
-        newDiv.addEventListener("mousedown", () => newDiv.style.backgroundColor = "cyan");
+        newDiv.addEventListener("mouseenter", () => newDiv.style.backgroundColor = "cyan");
         // newDiv.setAttribute("id", "main-child:hover");
     }
 }
 appendDivsTo(mainContainer);
+
+button.addEventListener("click", (event) => {
+    event.target.blur();
+    boxCount = prompt("Enter Box Count: ");
+    boxCount *= boxCount;
+    delete(appendDivsTo(mainContainer));
+    return appendDivsTo(mainContainer, boxCount);
+})
